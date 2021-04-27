82fff7b8ab606f9a30f0f75dfee0052512f8d16b
Luke Ellwood <ellwood.luke@gmail.com>
2021-04-27 17:34:36 +0100

    added keypress front end

--- app.js
+++ app.js
@@ -10,34 +10,58 @@ let snare = new Audio("sounds/snare.wav")
 let tom = new Audio("sounds/tom.wav")
 let crash = new Audio("sounds/crash.wav")
 
-
 let keys = document.body
 
 keys.addEventListener("keypress", (event) =>{
     if(event.code == "KeyH"){
       hihat.load()
       hihat.play()
+      document.getElementsByClassName("hihat")[0].style.opacity = 0.5;
+      onkeyup = () => {
+      document.getElementsByClassName("hihat")[0].style.opacity = 1;
+      }
   } else if(event.code == "KeyD"){
       kick.load()
       kick.play()
+      document.getElementsByClassName("kick")[0].style.opacity = 0.5;
+      onkeyup = () => {
+      document.getElementsByClassName("kick")[0].style.opacity = 1;
+      }
   } else if(event.code == "KeyJ"){
       openhat.load()
       openhat.play()
+      document.getElementsByClassName("openHi")[0].style.opacity = 0.5;
+      onkeyup = () => {
+      document.getElementsByClassName("openHi")[0].style.opacity = 1;
+      }
   } else if(event.code == "KeyK"){
       ride.load()
       ride.play()
+      document.getElementsByClassName("ride")[0].style.opacity = 0.5;
+      onkeyup = () => {
+      document.getElementsByClassName("ride")[0].style.opacity = 1;
+      }
   } else if(event.code == "KeyS"){
       snare.load()
       snare.play()
-  } else if(event.code == "KeyP"){
-      tink.load()
-      tink.play()
+      document.getElementsByClassName("snare")[0].style.opacity = 0.5;
+      onkeyup = () => {
+      document.getElementsByClassName("snare")[0].style.opacity = 1;
+      }
   } else if(event.code == "KeyU"){
       tom.load()
       tom.play()
+      document.getElementsByClassName("tom")[0].style.opacity = 0.5;
+      onkeyup = () => {
+      document.getElementsByClassName("tom")[0].style.opacity = 1;
+      }
   } else if(event.code == "KeyI"){
       crash.load()
       crash.play()
+      document.getElementsByClassName("crash")[0].style.opacity = 0.5;
+      onkeyup = () => {
+      document.getElementsByClassName("crash")[0].style.opacity = 1;
+      }
   }
 })
 
